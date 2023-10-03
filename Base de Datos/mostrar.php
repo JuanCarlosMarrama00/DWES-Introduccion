600a87342cb8d554dd55cd772b568128083fc4f3
JuanCarlosMarrama00 <marramamartinezjc@gmail.com>
2023-10-03 11:24:10 +0200

    He cambiado el ejercicio de mostrar añadiendo un where

--- Base de Datos/mostrar.php
+++ Base de Datos/mostrar.php
@@ -2,7 +2,7 @@
     require('conexion.php');
     require('usuario.php');
 
-    $resultado = $pdo->query("SELECT nombre_usuario FROM usuario");
+    $resultado = $pdo->query("SELECT nombre_usuario FROM usuario where id_usuario=2");
     while($registro = $resultado->fetch()) {
         echo "Nombre de Usuario: " . $registro['nombre_usuario'];
     }
