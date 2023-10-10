eacfbd04112f3aba50fd3820f3eed4e52dc39a19
JuanCarlosMarrama00 <marramamartinezjc@gmail.com>
2023-10-10 12:05:01 +0200

    He añadido caracteristicas al formulario de registrarse

--- Sesiones/registrarse.php
+++ Sesiones/registrarse.php
@@ -2,18 +2,22 @@
 require("conectar.php");
 
 $errores = [];
+$nombreUsuario = $password = $correo = "";
 
 if (isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["nombreUsuario"])) {
+    $nombreUsuario = $_POST["nombreUsuario"];
+    $correo = $_POST["correo"];
+    $password = $_POST["password"];
+    if (empty($nombreUsuario)) {
         $errores[] = "Ingresa un nombre de usuario";
     }
-    if (empty($_POST["correo"]) || !filter_var($_POST["correo"], FILTER_VALIDATE_EMAIL)) {
+    if (empty($correo) || !filter_var($correo, FILTER_VALIDATE_EMAIL)) {
         $errores[] = "No has ingresado un correo electrónico o tu email no es valido";
     }
-    if (empty($_POST["password"])) {
+    if (empty($password)) {
         $errores[] = "Por favor introduce una contraseña";
     }
-    if(strlen($_POST["password"])< 6) {
+    if (strlen($_POST["password"]) < 6) {
         $errores[] = "La contraseña debe tener más de 6 caracteres";
     }
     if ($_POST["password"] !== $_POST["repetirPassword"]) {
@@ -21,16 +25,24 @@ if (isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (empty($errores)) {
+        try {
         $pdoSt = $pdo->prepare('INSERT INTO users (username, email, password) VALUES (?,?,?)');
-        $nombreUsuario = $_POST["nombreUsuario"];
-        $correo = $_POST["correo"];
-        $password = $_POST["password"];
-
         $pdoSt->bindValue(1, $nombreUsuario);
         $pdoSt->bindValue(2, $correo);
         $pdoSt->bindValue(3, $password);
         $pdoSt->execute();
-        echo "Se ha registrado correctamente";
+        //$_SESSION["username"] = $nombreUsuario;
+        header("location:login.php");
+
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            if(strpos($error, 'users.username') !== false) {
+                $errores[] = "Este nombre de usuario ya existe";
+            } elseif(strpos($error, 'users.email') !== false) {
+                $errores[] = "Este email ya pertenece a un usuario";
+            }
+        }
+        
     }
 }
 ?>
@@ -43,33 +55,33 @@ if (isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica Pagina Registrarse php</title>
-    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <link rel="stylesheet" type="text/css" href="index.css" />
 </head>
 
 <body>
     <div id="pagina">
-    <h1>Formulario de registro</h1>
     <?php
-    if(isset($errores)) {
-        foreach($errores as $error) {
-            echo $error . "<br>";
+        if (isset($errores)) {
+            foreach ($errores as $error) {
+                echo $error . "<br>";
+            }
         }
-    }
-    ?>
-    <div id="formulario">
-    <form action="registrarse.php" method="post" enctype="multipart/form-data">
-        <p>Nombre de usuari@:</p>
-        <input type="text" name="nombreUsuario" maxlength="50"><br>
-        <p>Correo electrónico:</p>
-        <input type="email" name="correo"><br>
-        <p>Contraseña:</p>
-        <input type="password" name="password"><br>
-        <p>Confirma la contraseña:</p>
-        <input type="password" name="repetirPassword"><br><br>
-        <input type="submit" value="Registrarse" name="submit"><br>
-    </form>
-    </div>
-    <p>¿Ya eres miembro?<a href="login.php">Acceso a usuari@s</a></p>
+        ?>
+        <h1>Formulario de registro</h1>
+        <div id="formulario">
+            <form action="registrarse.php" method="post" enctype="multipart/form-data">
+                <p>Nombre de usuari@:</p>
+                <input type="text" name="nombreUsuario" maxlength="50" value=<?= $nombreUsuario ?>><br>
+                <p>Correo electrónico:</p>
+                <input type="email" name="correo" value=<?= $correo ?>><br>
+                <p>Contraseña:</p>
+                <input type="password" name="password"><br>
+                <p>Confirma la contraseña:</p>
+                <input type="password" name="repetirPassword"><br><br>
+                <input type="submit" value="Registrarse" name="submit"><br>
+            </form>
+        </div>
+        <p>¿Ya eres miembro?<a href="login.php">Acceso a usuari@s</a></p>
     </div>
 </body>
 
